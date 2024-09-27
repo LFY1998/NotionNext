197d0a4a1297f64f60f3cab8bafe9a5bcba3dc9e
Douhua <51777613+LFY1998@users.noreply.github.com>
2024-09-27 20:35:16 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -113,8 +113,8 @@ const CONFIG = {
   HEO_SOCIAL_CARD_TITLE_1: '豆花专属空间',
   HEO_SOCIAL_CARD_TITLE_2: '和爸爸妈妈成长的日常',
   HEO_SOCIAL_CARD_TITLE_3: '查看豆花变成猪咪的记录',
-  HEO_SOCIAL_CARD_TITLE_4: '（咬人记录）',
-  HEO_SOCIAL_CARD_URL: '',
+  //HEO_SOCIAL_CARD_TITLE_4: '（咬人记录）',
+  HEO_SOCIAL_CARD_URL: 'https://lfymay23ustc.xyz/',
 
   // *****  以下配置无效，只是预留开发 ****
   // 菜单配置
