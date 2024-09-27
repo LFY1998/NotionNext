c6d1faeeb470b22d004ea0dea85558cba4a61db4
Douhua <51777613+LFY1998@users.noreply.github.com>
2024-09-27 20:16:22 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -110,9 +110,9 @@ const CONFIG = {
   ],
 
   HEO_SOCIAL_CARD: true, // 是否显示右侧，点击加入社群按钮
-  HEO_SOCIAL_CARD_TITLE_1: '交流频道',
-  HEO_SOCIAL_CARD_TITLE_2: '加入我们的社群讨论分享',
-  HEO_SOCIAL_CARD_TITLE_3: '点击加入社群',
+  HEO_SOCIAL_CARD_TITLE_1: '豆花专属空间',
+  HEO_SOCIAL_CARD_TITLE_2: '成长的日常',
+  HEO_SOCIAL_CARD_TITLE_3: '查看豆花变成猪咪的记录',
   HEO_SOCIAL_CARD_URL: 'https://docs.tangly1024.com/article/how-to-question',
 
   // *****  以下配置无效，只是预留开发 ****
